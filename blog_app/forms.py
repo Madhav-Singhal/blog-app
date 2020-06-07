2ab6d9f33cb35f07c0ebe09273f504326e3f9b19
Madhav-Singhal <54695191+Madhav-Singhal@users.noreply.github.com>
2020-06-07 11:21:22 -0700

    Update forms.py

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -8,6 +8,8 @@ class PostForm(forms.ModelForm):
         fields = ['title','text', 'img']
 
 class CommentForm(forms.ModelForm):
+    comment= forms.CharField(label='', widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'text goes here', 'rows':'4', 'cols':'50'}))
+
 
     class Meta():
         model = AddComment
